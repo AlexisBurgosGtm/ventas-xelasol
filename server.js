a038c9e5f48c2569160b86e2fa146c4f5cdf0dd3
Alexis Burgos (gtm) <34293632+AlexisBurgosGtm@users.noreply.github.com>
2022-04-26 08:24:29 -0600

    s

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var routerapi = require('./router/routerapi');
 
-const PORT = process.env.PORT || 3690;
+const PORT = process.env.PORT || 3601;
 
 
 app.use(bodyParser.json());
